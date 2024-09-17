ed9edc2e8cb3c7105193c6e07835c7acab7e08a4
gidoo5331 <gideonbempong533@gmail.com>
2024-09-17 09:28:53 +0000

    added land sacpe

---
+++ app/landscape_gh/apps.py
@@ -0,0 +1,7 @@
+from django.apps import AppConfig
+
+
+class LandscapeGhConfig(AppConfig):
+    default_auto_field = 'django.db.models.BigAutoField'
+    name = 'landscape_gh'
+    verbose_name = 'Landscape GH'
